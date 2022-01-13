e02ad09c6b4702612987d29f46389dda83afb6e7
AshishKalra <ashishdotnetkalra@gmail.com>
2022-01-13 05:44:16 +0530

    Decorator Design Pattern POC

---
+++ DecoratorDesignPattern/DecoratorDesignPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Decorator.Menus;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMenu menu = new Menu(new List<IMenuItem>
+            {
+                new MenuItem("Soya Chaap",70),
+                new MenuItem("Pizza",130),
+                new MenuItem("Chowmein",50)
+            });
+
+            menu = new DiscountMenu(menu, 50);
+            menu = new DailySpecialMenu(menu, new MenuItem("Tea", 10, true));
+
+            foreach(IMenuItem menuItem in menu.Items)
+            {
+                Console.WriteLine(menuItem);
+            }
+        }
+    }
+}
